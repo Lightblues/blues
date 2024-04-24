a08c447e1921e563f33dd2ddbeff742c28c442cc
FrankShi <oldcitystal@gmail.com>
2024-04-24 21:49:06 +0800

    [feat] arxiv

--- userscripts/arxiv/arxiv.js
+++ userscripts/arxiv/arxiv.js
@@ -24,22 +24,41 @@
         button.style.top = '10px';
         button.style.right = '10px';
         button.style.zIndex = '1000';
-        button.style.opacity = '0.5'; // 设置透明度
+        button.style.opacity = '0.8'; // 设置透明度
         button.style.cursor = 'move'; // 设置光标为移动模式
 
+        // 固定大小? 不然移动之后左边还停在右边框
+        button.style.width = '150px';
+        // button.style.height = '40px';
+
+        
         // 拖动功能的实现
         let posX = 0, posY = 0, mouseX = 0, mouseY = 0;
+        // 在脚本中添加一个标志来判断用户是否拖动了按钮
+        let isDragging = false;
 
         const mouseDownHandler = function(e) {
+            // 阻止事件的默认行为
+            e.preventDefault();
+
             // 获取初始位置
             mouseX = e.clientX;
             mouseY = e.clientY;
+
+            // 当mousedown时，先设置拖动标志为false
+            isDragging = false;
 
             document.addEventListener('mousemove', mouseMoveHandler);
             document.addEventListener('mouseup', mouseUpHandler);
         };
 
+
+
+
         const mouseMoveHandler = function(e) {
+            // 设置拖动标志为true
+            isDragging = true;
+
             // 计算移动距离
             const dx = e.clientX - mouseX;
             const dy = e.clientY - mouseY;
@@ -60,12 +79,22 @@
             // 移除事件监听
             document.removeEventListener('mousemove', mouseMoveHandler);
             document.removeEventListener('mouseup', mouseUpHandler);
+
+            // 如果mouseup时，isDragging为false，则认为是单击，执行跳转
+            if (!isDragging) {
+                window.location.href = `https://papers.cool/arxiv/${arxivId}`;
+            }
         };
 
         button.addEventListener('mousedown', mouseDownHandler);
 
-        button.onclick = function() {
-            window.location.href = `https://papers.cool/arxiv/${arxivId}`;
+        // button.onclick = function() {
+        //     window.location.href = `https://papers.cool/arxiv/${arxivId}`;
+        // };
+        // 修改button的点击事件，移除原来的跳转逻辑
+        button.onclick = function(e) {
+            // 阻止事件冒泡，避免mouseup事件中的跳转逻辑被触发
+            e.stopPropagation();
         };
 
         document.body.appendChild(button);
